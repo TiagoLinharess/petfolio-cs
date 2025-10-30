1d990956a77bb251db0b33d989b9a5a9cdac3652
Tiago Linhares <tiagolinharessouza@gmail.com>
2025-10-30 08:28:36 -0300

    Creates project

---
+++ Petfolio.API/Controllers/PetController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pets.Register;
+using Petfolio.Application.UseCases.Pets.Update;
+using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
+        public IActionResult Create([FromBody] RequestPetJson request)
+        {
+            var useCase = new RegisterPetUseCase();
+            var response = useCase.Execute(request);
+
+            return Created(string.Empty, response);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Update([FromRoute] int id, [FromBody] RequestPetJson request)
+        {
+            var useCase = new UpdatePetUseCase();
+            useCase.Execute(id, request);
+
+            return NoContent();
+        }
+    }
+}
